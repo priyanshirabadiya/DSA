7b633ab65fad359aaf4010f17859327b7cd8976c
priyanshirabadiya <139095730+priyanshirabadiya@users.noreply.github.com>
2023-08-16 22:38:56 +0530

    new

--- operator.c
+++ operator.c
@@ -3,7 +3,7 @@ int main()
 {
     int a=1,b=2,d=1,p=3,c;
     c = ++a || b-- && d-- || p--;
-    printf("\na:%d ,b:%d  d:%d, p:%d ,c:%d",a,b,d,p,c);
+    printf("\na:%d ,b:%d   d:%d, p:%d ,c:%d",a,b,d,p,c);
 
   //  c=a&&b;
    // printf("a:%d ,b:%d ,c:%d",a,b,c);
